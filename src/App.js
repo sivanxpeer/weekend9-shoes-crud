bd96ccedb027d0d1d3376d09f5cc190b89d41bf3
sivanxpeer <sivan.x.peer@gmail.com>
2021-12-26 02:48:07 +0200

    commit

--- src/App.js
+++ src/App.js
@@ -2,22 +2,18 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  componentDidMount = async () => {
+    try {
+      const { data } = await dataApi.get('/products');
+      console.log(data);
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
     </div>
   );
 }
---
+++ src/DataApi/DataApi.js
@@ -0,0 +1,2 @@
+const dataApi = axios.create({baseUrl:'https://61c7b1a2903185001754749c.mockapi.io'});
+export default dataApi;
